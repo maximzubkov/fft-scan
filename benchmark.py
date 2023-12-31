b58d37184caf0357224a164f49606fb7edef1b09
maximzubkov <maksim.zubkov@epfl.ch>
2023-12-31 16:42:46 +0100

    Basic benchmark script

---
+++ benchmark.py
@@ -0,0 +1,33 @@
+import torch
+import torch.utils.benchmark as benchmark
+
+from src.naive import pscan_naive
+from src.fft_simple import pscan_fft_simple
+from src.fft_efficeint import pscan_fft_efficient
+
+N, T, D = 20, 128, 378
+
+A = torch.randn(N, T).requires_grad_() / 10 + 1
+X = torch.randn(N, T, D).requires_grad_() / 1000
+
+t_naive = benchmark.Timer(
+    stmt='pscan_naive(A, X)',
+    setup='from src.naive import pscan_naive',
+    globals={'A': A, 'X': X}
+)
+
+t_fft_simple = benchmark.Timer(
+    stmt='pscan_fft_simple(A, X)',
+    setup='from src.fft_simple import pscan_fft_simple',
+    globals={'A': A, 'X': X}
+)
+
+t_fft_efficient = benchmark.Timer(
+    stmt='pscan_fft_efficient(A, X)',
+    setup='from src.fft_efficeint import pscan_fft_efficient',
+    globals={'A': A, 'X': X}
+)
+
+print(t_naive.timeit(100))
+print(t_fft_simple.timeit(100))
+print(t_fft_efficient.timeit(100))
