edcbf4928cff58d9d7f1453d0ad7896340112daa
maximzubkov <maksim.zubkov@epfl.ch>
2024-01-01 15:57:22 +0100

    Loop over different matrices

--- benchmark.py
+++ benchmark.py
@@ -44,83 +44,55 @@ approach2fn = {
 if __name__ == "__main__":
     approach2timing_forward = defaultdict(dict)
     approach2timing_backward = defaultdict(dict)
-    approach2memory_forward = defaultdict(dict)
-    approach2memory_backward = defaultdict(dict)
 
     Ts = [128, 256, 512, 1024, 2048, 4096, 8192]
-    N, D = 4, 1024
+    N, D = 2, 2048
     for T in tqdm(Ts):
-        globals_ = {
-            'A': torch.randn(N, T, device='cuda').requires_grad_() / 10 + 1,
-            'X': torch.randn(N, T, D, device='cuda').requires_grad_() / 10,
-            'pscan_forward': pscan_forward,
-            'pscan_backward': pscan_backward
-        }
-        for approach, setup in tqdm(approach2setup.items()):
-            globals_["fn"] = approach2fn[approach]
-            if approach == "ff":
-                globals_["Y_init"] = torch.randn(N, D, device='cuda').requires_grad_() / 1000
-            else:
-                globals_["Y_init"] = None
+        timing_forward, timing_backward = [], []
+        for _ in range(10):
+            globals_ = {
+                'A': torch.randn(N, T, device='cuda').requires_grad_() / 10 + 1,
+                'X': torch.randn(N, T, D, device='cuda').requires_grad_() / 10,
+                'pscan_forward': pscan_forward,
+                'pscan_backward': pscan_backward
+            }
+            for approach, setup in tqdm(approach2setup.items()):
+                globals_["fn"] = approach2fn[approach]
+                if approach == "ff":
+                    globals_["Y_init"] = torch.randn(N, D, device='cuda').requires_grad_() / 10
+                else:
+                    globals_["Y_init"] = None
 
-            t_forward = benchmark.Timer(
-                stmt=stmt_forward,
-                setup=setup,
-                globals=globals_
-            )
-            try:
-                timing_forward = t_forward.timeit(100).median
-            except:
-                timing_forward = None
+                t_forward = benchmark.Timer(
+                    stmt=stmt_forward,
+                    setup=setup,
+                    globals=globals_
+                )
+                try:
+                    timing_forward += t_forward.timeit(50).times
+                except:
+                    pass
 
-            t_backward = benchmark.Timer(
-                stmt=stmt_backward,
-                setup=setup,
-                globals=globals_
-            )
-            try:
-                timing_backward = t_backward.timeit(100).median
-            except:
-                timing_backward = None
+                t_backward = benchmark.Timer(
+                    stmt=stmt_backward,
+                    setup=setup,
+                    globals=globals_
+                )
+                try:
+                    timing_backward += t_backward.timeit(50).times
+                except:
+                    pass
 
-            approach2timing_forward[approach][T] = timing_forward
-            approach2timing_backward[approach][T] = timing_backward
-
-            with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True, profile_memory=True) as prof:
-                pscan_forward(**globals)
-
-            stats = prof.key_averages().table(sort_by="cuda_time_total", row_limit=1)
-            parts = stats.split("\n")[-5].split()
-            memory_forward = -float(parts[-3])
-            if parts[-2] == "Mb":
-                memory_forward = memory_forward / 1000
-
-            with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True, profile_memory=True) as prof:
-                pscan_backward(**globals)
-
-            stats = prof.key_averages().table(sort_by="cuda_time_total", row_limit=1)
-            parts = stats.split("\n")[-5].split()
-            memory_backward = -float(parts[-3])
-            if parts[-2] == "Mb":
-                memory_backward = memory_backward / 1000
-
-            approach2memory_forward[approach][T] = memory_forward
-            approach2memory_backward[approach][T] = memory_backward
+        approach2timing_forward[approach][T] = timing_forward if timing_forward else None
+        approach2timing_backward[approach][T] = timing_backward if timing_backward else None
 
         print(json.dumps({k: dict(v) for k, v in approach2timing_forward.items()}, indent=2))
         print(json.dumps({k: dict(v) for k, v in approach2timing_backward.items()}, indent=2))
-        print(json.dumps({k: dict(v) for k, v in approach2memory_forward.items()}, indent=2))
-        print(json.dumps({k: dict(v) for k, v in approach2memory_backward.items()}, indent=2))
+
     approach2timing_forward = {k: dict(v) for k, v in approach2timing_forward.items()}
     approach2timing_backward = {k: dict(v) for k, v in approach2timing_backward.items()}
-    approach2memory_forward = {k: dict(v) for k, v in approach2memory_forward.items()}
-    approach2memory_backward = {k: dict(v) for k, v in approach2memory_backward.items()}
 
     with open("timing_forward.json", "w") as f:
         f.write(json.dumps(approach2timing_forward, indent=2))
     with open("timing_backward.json", "w") as f:
         f.write(json.dumps(approach2timing_backward, indent=2))
-    with open("memory_forward.json", "w") as f:
-        f.write(json.dumps(approach2memory_forward, indent=2))
-    with open("memory_backward.json", "w") as f:
-        f.write(json.dumps(approach2memory_backward, indent=2))
