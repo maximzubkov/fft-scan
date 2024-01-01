bbd2005be00d0ec235f01fa6efefe12b83f5e9e2
maximzubkov <maksim.zubkov@epfl.ch>
2024-01-01 15:19:02 +0100

    Add memory estiamtions

--- benchmark.py
+++ benchmark.py
@@ -1,5 +1,6 @@
 import torch
 import torch.utils.benchmark as benchmark
+from torch.profiler import profile, record_function, ProfilerActivity
 
 from collections import defaultdict
 import json
@@ -19,7 +20,7 @@ stmt_forward = "pscan_forward(fn, A, X, Y_init)"
 def pscan_backward(fn, A, X, Y_init=None):
     Y = fn(A, X) if Y_init is None else fn(A, X, Y_init)
     loss = Y.sum()
-    loss.backward()
+    loss.backward(retain_graph=True)
 
 stmt_backward = "pscan_backward(fn, A, X, Y_init)"
 
@@ -43,18 +44,22 @@ approach2fn = {
 if __name__ == "__main__":
     approach2timing_forward = defaultdict(dict)
     approach2timing_backward = defaultdict(dict)
+    approach2memory_forward = defaultdict(dict)
+    approach2memory_backward = defaultdict(dict)
 
-    Ts = [128, 256, 512, 1024, 2048, 4096, 8192, 12288]
-    N, D = 16, 1024
+    Ts = [128, 256, 512, 1024, 2048, 4096, 8192]
+    N, D = 4, 1024
     for T in tqdm(Ts):
         globals_ = {
-            'A': torch.randn(N, T).requires_grad_() / 10 + 1, 
-            'X': torch.randn(N, T, D).requires_grad_() / 1000, 
+            'A': torch.randn(N, T, device='cuda').requires_grad_() / 10 + 1,
+            'X': torch.randn(N, T, D, device='cuda').requires_grad_() / 10,
+            'pscan_forward': pscan_forward,
+            'pscan_backward': pscan_backward
         }
         for approach, setup in tqdm(approach2setup.items()):
             globals_["fn"] = approach2fn[approach]
             if approach == "ff":
-                globals_["Y_init"] = torch.randn(N, D).requires_grad_() / 1000
+                globals_["Y_init"] = torch.randn(N, D, device='cuda').requires_grad_() / 1000
             else:
                 globals_["Y_init"] = None
 
@@ -80,13 +85,42 @@ if __name__ == "__main__":
 
             approach2timing_forward[approach][T] = timing_forward
             approach2timing_backward[approach][T] = timing_backward
-    
+
+            with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True, profile_memory=True) as prof:
+                pscan_forward(**globals)
+
+            stats = prof.key_averages().table(sort_by="cuda_time_total", row_limit=1)
+            parts = stats.split("\n")[-5].split()
+            memory_forward = -float(parts[-3])
+            if parts[-2] == "Mb":
+                memory_forward = memory_forward / 1000
+
+            with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True, profile_memory=True) as prof:
+                pscan_backward(**globals)
+
+            stats = prof.key_averages().table(sort_by="cuda_time_total", row_limit=1)
+            parts = stats.split("\n")[-5].split()
+            memory_backward = -float(parts[-3])
+            if parts[-2] == "Mb":
+                memory_backward = memory_backward / 1000
+
+            approach2memory_forward[approach][T] = memory_forward
+            approach2memory_backward[approach][T] = memory_backward
+
+        print(json.dumps({k: dict(v) for k, v in approach2timing_forward.items()}, indent=2))
+        print(json.dumps({k: dict(v) for k, v in approach2timing_backward.items()}, indent=2))
+        print(json.dumps({k: dict(v) for k, v in approach2memory_forward.items()}, indent=2))
+        print(json.dumps({k: dict(v) for k, v in approach2memory_backward.items()}, indent=2))
     approach2timing_forward = {k: dict(v) for k, v in approach2timing_forward.items()}
     approach2timing_backward = {k: dict(v) for k, v in approach2timing_backward.items()}
+    approach2memory_forward = {k: dict(v) for k, v in approach2memory_forward.items()}
+    approach2memory_backward = {k: dict(v) for k, v in approach2memory_backward.items()}
 
     with open("timing_forward.json", "w") as f:
         f.write(json.dumps(approach2timing_forward, indent=2))
     with open("timing_backward.json", "w") as f:
         f.write(json.dumps(approach2timing_backward, indent=2))
-            
-                
+    with open("memory_forward.json", "w") as f:
+        f.write(json.dumps(approach2memory_forward, indent=2))
+    with open("memory_backward.json", "w") as f:
+        f.write(json.dumps(approach2memory_backward, indent=2))
--- src/cumsum.py
+++ src/cumsum.py
@@ -9,24 +9,22 @@ def pscan_cumsum(A, X):
     # A_log \in [N x T]
     A_log = torch.log(A.to(dtype=torch.cfloat))
 
-    A_log_flipped = torch.flip(A_log, dims=(1,))
-    UA = torch.flip(A_log_flipped.cumsum(dim=-1), dims=(1,))
+    CS = A_log.cumsum(dim=-1)
+    # A_log.sum(dim=-1) + A_log - A_log.cumsum(dim=-1) = A_log[:, ::-1].cumsum(dim=-1)[:, ::-1]
+    UA = CS[:, -1].unsqueeze(-1) + A_log - CS
 
     W = UA
-    W = W.real
-    W_max = W.max()
-    e_W = torch.exp(W - W_max)
+    W_max = W.real.max()
+    e_W = torch.exp(W - W_max).real
     e_W = e_W.unsqueeze(-1)
 
     V = -UA + A_log
-    V = V.real
-    V_max = V.max()
-    e_V = torch.exp(V - V_max)
+    V_max = V.real.max()
+    e_V = torch.exp(V - V_max).real
     e_V = e_V.unsqueeze(-1)
     Y_ = e_V * torch.cumsum(e_W * X, dim=1) * (torch.exp(V_max + W_max))
 
     # After exp we no longer have complex components
-    Y_ = Y_.real
     Y_ = torch.cat([torch.zeros(N, 1, D, device=device), Y_[:, :-1, :]], dim=1) 
     Y = Y_ + X
     return Y    
